35757a3da88e5010d0735b76791bc04437ca269a
Bukhari Abdulhakeem <114989677+Kingbah001@users.noreply.github.com>
2025-07-17 13:17:45 -0700

    added the rating section

--- style.js
+++ style.js
@@ -8,3 +8,30 @@ function Addclass() {
     backdrop.classList.toggle('showlinks')
 }
 
+
+//code for the ratings 
+let stars = document.querySelectorAll(".ratings span");
+let products = document.querySelectorAll(".ratings");
+let ratings = [];
+
+for(let star of stars){
+    star.addEventListener("click", function(){
+        let children = star.parentElement.children;
+        for(let child  of children){
+            if(child.getAttribute("data-clicked")){
+                return false;
+            }
+        }
+
+        this.setAttribute("data-clicked", "true");
+        let rating = this.dataset.rating;
+        let productid = this.parentElement.dataset.productid;
+
+        let data = {
+            "stars": rating,
+            "product-id": productid
+        }
+        ratings.push(data);
+        localStorage.setItem("rating", JSON.stringify(ratings));
+    });
+};
